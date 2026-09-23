9268e902cae52a6752574f714283c791386a995e
Suco De Fruta <ryanlopomo@gmail.com>
2026-09-22 22:18:24 -0300

    Add Google site verification meta tag
    Added Google site verification meta tag to the layout.

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <meta name="google-site-verification" content="YOMp1uB8GBdP7FeG1ZwsRL5uix8AO6DCLkoKr-K6I9g" />
+        {children}
+      </body>
     </html>
   );
 }
